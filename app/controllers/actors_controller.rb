0b443947dc462ebaed4d4eb8de583f0e18897753
drdpatel <165212603+drdpatel@users.noreply.github.com>
2024-05-22 19:28:32 +0000

    actor update partial

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -15,13 +15,17 @@ class ActorsController < ApplicationController
  
  def destroy
   the_id = params.fetch("an_id")
-
   matching_records = Actor.where({ :id => the_id })
 
   the_actor = matching_records.at(0)
 
   the_actor.destroy
   redirect_to("/actors")
+
+end
+
+
+def modify
 
 end
 
